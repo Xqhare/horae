c0bbde38e146a490153d7044ab98525c80a5fa6a
Xqhare <xqhare@web.de>
2024-10-20 00:31:26 +0200

    feat: testing

--- tests/dev.rs
+++ tests/dev.rs
@@ -5,9 +5,11 @@ use horae::Utc;
 
 #[test]
 fn creation_datetime_utc() {
+    let instant = std::time::Instant::now();
     let dt = Utc::now();
     println!("{}", dt);
-    sleep(std::time::Duration::from_nanos(10));
+    println!("Microseconds elapsed: {}", instant.elapsed().as_micros());
+    sleep(std::time::Duration::from_micros(10));
     let dt2 = Utc::now();
     println!("{}", dt2);
 }
