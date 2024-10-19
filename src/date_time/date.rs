c4e4b5e6fd4d31ba8ab91c9ec89427c2ab56a8bc
Xqhare <xqhare@web.de>
2024-10-19 21:45:54 +0200

    feat: added from and display

--- src/date_time/date.rs
+++ src/date_time/date.rs
@@ -4,3 +4,17 @@ pub struct Date {
     month: u8,
     day: u8,
 }
+
+impl Date {
+    pub fn from_ymd(year: u16, month: u8, day: u8) -> Date {
+        Date { year, month, day }
+    }
+}
+
+// Display implementation
+
+impl std::fmt::Display for Date {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:04}-{:02}-{:02}", self.year, self.month, self.day)
+    }
+}
