dab573c4eb6e7e6f9ae885df312c0928aa9aca28
Xqhare <xqhare@web.de>
2024-10-19 21:46:13 +0200

    feat: added DateTime creation

--- src/date_time/mod.rs
+++ src/date_time/mod.rs
@@ -1,3 +1,4 @@
+use common::{make_now_date, make_now_time};
 use date::Date;
 use time::Time;
 
@@ -5,9 +6,9 @@ use crate::time_zones::TimeZone;
 
 mod date;
 mod time;
+mod common;
 
-const SECONDS_IN_DAY: f64 = 86_400.0;
-const DAYS_IN_YEAR_APPROX: f64 = 365.0;
+
 
 pub struct DateTime {
     date: Date,
@@ -18,16 +19,31 @@ pub struct DateTime {
 
 impl DateTime {
     pub fn now() -> DateTime {
-        let unix_timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs_f64();
-        let days_since_epoch = unix_timestamp / SECONDS_IN_DAY;
-        let years_since_epoch = (days_since_epoch / DAYS_IN_YEAR_APPROX).floor();
-        let leap_years
+        let (date, timestamp, unix_timestamp) = make_now_date();
+        let time = make_now_time(timestamp);
         DateTime {
-            date: Date::now(),
-            time: Time::now(),
+            date,
+            time,
             unix_timestamp,
             timezone: TimeZone::UTC,
         }
     }
     
+    pub fn with_timezone(timezone: TimeZone) -> DateTime {
+        let mut out = DateTime::now();
+        out.set_timezone(timezone);
+        out
+    }
+
+    fn set_timezone(&mut self, timezone: TimeZone) {
+        self.timezone = timezone;
+    }
+}
+
+// Display implementation
+
+impl std::fmt::Display for DateTime {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{} {}", self.date, self.time)
+    }
 }
