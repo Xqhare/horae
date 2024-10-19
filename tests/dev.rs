d6990c750924d64d23c8109ab4433c4f6afe9346
Xqhare <xqhare@web.de>
2024-10-19 23:45:46 +0200

    chore: testing it

--- tests/dev.rs
+++ tests/dev.rs
@@ -1,8 +1,13 @@
 
+use std::thread::sleep;
+
 use horae::Utc;
 
 #[test]
 fn creation_datetime_utc() {
     let dt = Utc::now();
     println!("{}", dt);
+    sleep(std::time::Duration::from_nanos(10));
+    let dt2 = Utc::now();
+    println!("{}", dt2);
 }
