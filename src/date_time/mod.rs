c3d3ff57584d1de9c9e5b606f295875606270394
Xqhare <xqhare@web.de>
2024-10-20 16:24:17 +0200

    feat: started work on with_timezone

--- src/date_time/mod.rs
+++ src/date_time/mod.rs
@@ -29,10 +29,59 @@ impl DateTime {
         }
     }
     
-    pub fn with_timezone(timezone: TimeZone) -> DateTime {
-        let mut out = DateTime::now();
-        out.set_timezone(timezone);
-        out
+    pub fn with_timezone(&mut self, timezone: TimeZone) {
+        let (utc_offset_hours, utc_offset_minutes) = {
+            let tmp = timezone.get_utc_offset();
+            let hours = tmp.floor() as i16;
+            let minutes: u8 = {
+                match tmp.fract() {
+                    // These are the only fractions to exist - For now I am sure
+                    f if f == 0.0 => 0,
+                    f if f == 0.25 => 15,
+                    f if f == 0.5 => 30,
+                    f if f == 0.75 => 45,
+                    _ => 0,
+                }
+            };
+            (hours, minutes)
+        };
+        self.set_timezone(timezone);
+
+        // First calculate new time, if hour is negative, go back 1 day
+        // TODO what about years?
+        if utc_offset_hours.is_positive() {
+            let tmp_hour_bind = self.time.hour as i16 + utc_offset_hours;
+            if tmp_hour_bind >= 24 {
+                // next day && possibly next month && possibly next year
+            } else {
+                self.time.hour = TryInto::<u8>::try_into(tmp_hour_bind).expect("Everything checked!");
+            }
+            let tmp_minute_bind = self.time.minute + utc_offset_minutes;
+            if tmp_minute_bind >= 60 {
+                self.time.minute = tmp_minute_bind - 60;
+                self.time.hour += 1;
+            } else {
+                self.time.minute = tmp_minute_bind;
+            }
+        // TODO
+        } else {
+            if self.time.hour as i16 >= utc_offset_hours {
+                // Same day
+                self.time.hour = self.time.hour - TryInto::<u8>::try_into(utc_offset_hours).expect("Everything checked!");
+                if self.time.minute >= utc_offset_minutes {
+                    // Same hour
+                    self.time.minute -= utc_offset_minutes;
+                } else {
+                    // Different hour
+                    self.time.hour -= 1;
+                    let tmp_minute_bind = utc_offset_minutes - self.time.minute;
+                    self.time.minute = 60 - tmp_minute_bind;
+                }
+            } else {
+                // +- offset minutes are not handled yet! -> Could still be the same day
+            }
+        }
+        
     }
 
     fn set_timezone(&mut self, timezone: TimeZone) {
