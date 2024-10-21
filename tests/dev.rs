d677ec4bff84776d3fe40b5b92a2ceae3e532d2d
Xqhare <xqhare@web.de>
2024-10-21 17:25:45 +0200

    chore: shortend the names of the rollover tests

--- tests/dev.rs
+++ tests/dev.rs
@@ -73,7 +73,7 @@ fn print_timezones() {
 }
 
 #[test]
-fn from_ymd_hms_with_timezone_utc() {
+fn from_ymd_hms_timezone_utc() {
     // utc(+00:00)
     let utc = Utc::from_ymd_hms_timezone(2021, 02, 25, 13, 59, 59, TimeZone::Utc);
     assert_eq!("2021-02-25 13:59:59.000", utc.to_string());
@@ -81,7 +81,7 @@ fn from_ymd_hms_with_timezone_utc() {
 
 // no rollover positive
 #[test]
-fn from_ymd_hms_with_timezone_positive() {
+fn timezone_positive() {
     // CEST(+02:00)
     let cest = Utc::from_ymd_hms_timezone(2021, 02, 25, 13, 59, 59, TimeZone::CentralEuropeanSummerTime);
     assert_eq!("2021-02-25 15:59:59.000", cest.to_string());
@@ -89,7 +89,7 @@ fn from_ymd_hms_with_timezone_positive() {
 
 // no rollover negative
 #[test]
-fn from_ymd_hms_with_timezone_negative() {
+fn timezone_negative() {
     // Marquesas Islands =-09:30
     let mart = Utc::from_ymd_hms_timezone(2021, 02, 25, 13, 59, 59, TimeZone::MarquesasIslandsTime);
     assert_eq!("2021-02-25 04:29:59.000", mart.to_string());
@@ -97,21 +97,21 @@ fn from_ymd_hms_with_timezone_negative() {
 
 // negative rollover
 #[test]
-fn from_ymd_hms_with_timezone_negative_rollover_year() {
+fn timezone_negative_rollover_year() {
     // Marquesas Islands =-09:30
     let mart = Utc::from_ymd_hms_timezone(2021, 01, 01, 0, 0, 59, TimeZone::MarquesasIslandsTime);
     assert_eq!("2020-12-31 15:30:59.000", mart.to_string());
 }
 
 #[test]
-fn from_ymd_hms_with_timezone_negative_rollover_month() {
+fn timezone_negative_rollover_month() {
     // Marquesas Islands =-09:30
     let mart = Utc::from_ymd_hms_timezone(2021, 02, 01, 0, 0, 59, TimeZone::MarquesasIslandsTime);
     assert_eq!("2021-01-31 15:30:59.000", mart.to_string());
 }
 
 #[test]
-fn from_ymd_hms_with_timezone_negative_rollover_day() {
+fn timezone_negative_rollover_day() {
     // Marquesas Islands =-09:30
     let mart = Utc::from_ymd_hms_timezone(2021, 02, 25, 0, 0, 59, TimeZone::MarquesasIslandsTime);
     assert_eq!("2021-02-24 15:30:59.000", mart.to_string());
@@ -119,21 +119,21 @@ fn from_ymd_hms_with_timezone_negative_rollover_day() {
 
 // positive rollover
 #[test]
-fn from_ymd_hms_with_timezone_positive_rollover_year() {
+fn timezone_positive_rollover_year() {
     //Rollover positive Chatham(+13.75)
     let chatham = Utc::from_ymd_hms_timezone(2021, 12, 31, 23, 59, 59, TimeZone::ChathamDaylightTime);
     assert_eq!("2022-01-01 13:44:59.000", chatham.to_string());
 }
 
 #[test]
-fn from_ymd_hms_with_timezone_positive_rollover_month() {
+fn timezone_positive_rollover_month() {
     //Rollover positive Chatham(+13.75)
-    let chatham = Utc::from_ymd_hms_timezone(2021, 03, 29, 23, 59, 59, TimeZone::ChathamDaylightTime);
+    let chatham = Utc::from_ymd_hms_timezone(2021, 03, 31, 23, 59, 59, TimeZone::ChathamDaylightTime);
     assert_eq!("2021-04-01 13:44:59.000", chatham.to_string());
 }
 
 #[test]
-fn from_ymd_hms_with_timezone_positive_rollover_day() {
+fn timezone_positive_rollover_day() {
     //Rollover positive Chatham(+13.75)
     let chatham = Utc::from_ymd_hms_timezone(2021, 03, 28, 23, 59, 59, TimeZone::ChathamDaylightTime);
     assert_eq!("2021-03-29 13:44:59.000", chatham.to_string());
