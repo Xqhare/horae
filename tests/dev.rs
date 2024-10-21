35268f5f8088f137e181608373cf58d8ad8ed0f4
Xqhare <xqhare@web.de>
2024-10-21 16:34:58 +0200

    feat: debugging and creation_time_performance added

--- tests/dev.rs
+++ tests/dev.rs
@@ -12,6 +12,32 @@ fn creation_datetime_utc() {
     println!("{}", dt2);
 }
 
+// 300k ns measured once
+// 400k ns measured once
+#[test]
+fn creation_time_performance() {
+    let mut biggest_dur = 0;
+    let mut loop_count = 0;
+    let mut duration_sum = 0;
+    for _ in 0..100_000_000 {
+        let instant = std::time::Instant::now();
+        let _dt = Utc::now();
+        let tmp = instant.elapsed().as_nanos();
+
+        if tmp > biggest_dur {
+            biggest_dur = tmp;
+        }
+        loop_count += 1;
+        duration_sum += tmp;
+        if tmp > 500_000 {
+            println!("Nanoseconds elapsed: {} in loop {}", tmp, loop_count);
+        }
+        assert!(tmp < 500_000);
+    }
+    println!("Average nanoseconds elapsed: {}", duration_sum / loop_count);
+    println!("Most nanoseconds elapsed: {}", biggest_dur);
+}
+
 #[test]
 fn creation_datetime_utc_with_timezone() {
     let instant = std::time::Instant::now();
@@ -25,12 +51,13 @@ fn creation_datetime_utc_with_timezone() {
 
 #[test]
 fn from_ymd_hms_with_timezone() {
-    let instant = std::time::Instant::now();
     let dt = Utc::from_ymd_hms_timezone(2021, 12, 31, 23, 59, 59, TimeZone::CentralEuropeanSummerTime);
-    println!("Rollover positive CEST: {}", dt);
-    println!("Microseconds elapsed: {}", instant.elapsed().as_micros());
+    // Rollover positive CEST(+02:00)
+    assert_eq!("2022-01-01 02:59:59.000", dt.to_string());
+
     let chatham = Utc::from_ymd_hms_timezone(2021, 12, 31, 23, 59, 59, TimeZone::ChathamDaylightTime);
-    println!("Rollover positive Chatham(+13.75): {}", chatham);
+    //Rollover positive Chatham(+13.75)
+    assert_eq!("2022-01-01 13:44:59.000", chatham.to_string());
 }
 
 #[test]
