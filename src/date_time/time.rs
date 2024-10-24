0e88bb487aec32b55dbf222f5b829e5beb23aa4e
Xqhare <xqhare@web.de>
2024-10-24 17:00:12 +0200

    feat: added formatter, slight internal api changes

--- src/date_time/time.rs
+++ src/date_time/time.rs
@@ -1,3 +1,5 @@
+use crate::tokenizer::{tokenize, Token, Unit};
+
 #[derive(Debug, Copy, Clone)]
 pub struct Time {
     pub hour: u8,
@@ -6,8 +8,20 @@ pub struct Time {
     pub subseconds: u64,
 }
 
-impl Time {
-    pub fn from_hms(hour: u8, minute: u8, second: u8) -> Time {
+impl From<(u8, u8, u8, f64)> for Time {
+    fn from((hour, minute, second, subseconds): (u8, u8, u8, f64)) -> Time {
+        let tmp = subseconds.fract() * 1_000_000_000.0;
+        Time {
+            hour,
+            minute,
+            second,
+            subseconds: tmp.floor() as u64,
+        }
+    }
+}
+
+impl From<(u8, u8, u8)> for Time {
+    fn from((hour, minute, second): (u8, u8, u8)) -> Time {
         Time {
             hour,
             minute,
@@ -15,15 +29,48 @@ impl Time {
             subseconds: 0,
         }
     }
+}
 
-    pub fn from_hmsns(hour: u8, minute: u8, second: u8, rest: f64) -> Time {
-        let tmp = rest.fract() * 1_000_000_000.0;
-        Time {
-            hour,
-            minute,
-            second,
-            subseconds: tmp.floor() as u64,
+impl Time {
+    pub fn format(&self, formatter: &str) -> String {
+        let format_tokens = tokenize(formatter);
+        let mut formatted_string = String::new();
+        for token in format_tokens {
+            match token {
+                Token::Unit(unit) => match unit {
+                    Unit::Millisecond => {
+                        formatted_string.push_str(&format!("{:03}", self.subseconds));
+                    },
+                    Unit::ShortSecond => {
+                        formatted_string.push_str(&format!("{:01}", self.second));
+                    },
+                    Unit::Second => {
+                        formatted_string.push_str(&format!("{:02}", self.second));    
+                    },
+                    Unit::ShortMinute => {
+                        formatted_string.push_str(&format!("{:01}", self.minute));
+                    },
+                    Unit::Minute => {
+                        formatted_string.push_str(&format!("{:02}", self.minute));
+                    },
+                    Unit::ShortHour => {
+                        formatted_string.push_str(&format!("{:01}", self.hour));
+                    },
+                    Unit::Hour => {
+                        formatted_string.push_str(&format!("{:02}", self.hour));
+                    },
+                    // Dont want to intruduce an error state now...
+                    _ => {
+                        formatted_string.push_str(" Time only supports millisecond, second, minute, and hour ");
+                    }
+                },
+                Token::Separator(separator) => {
+                    formatted_string.push_str(&separator.separator_symbol);
+                }
+            }
         }
+
+        formatted_string
     }
 }
 
